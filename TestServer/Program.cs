8a60bb003101d3d68274f4cef16fa6f93d27ba69
parrymay <90844727+parrymay@users.noreply.github.com>
2022-07-06 14:53:27 +0300

    init3

--- TestServer/Program.cs
+++ TestServer/Program.cs
@@ -11,20 +11,19 @@ using Task_2;
 //писатели писали только последовательно и никогда одновременно;
 //пока писатели добавляют и пишут, читатели должны ждать окончания записи.
 
-
 // Визуализация Задачи 1 (раскомментировать код в классе Server)
-//for (int i = 0; i < 10; i++)
-//{
-//    Thread thread_1 = new(()=>Server.GetCount());
-//    Thread thread_2 = new(() => Server.AddToCount(1));
-//    Thread thread_3 = new(() => Server.GetCount());
-//    thread_1.Name = $"thread {i}";
-//    thread_2.Name = $"thread {i + 1}";
-//    thread_3.Name = $"thread {i + 2}";
-//    thread_1.Start();
-//    thread_2.Start();
-//    thread_3.Start();
-//}
+for (int i = 0; i < 10; i++)
+{
+    Thread thread_1 = new(() => Server.GetCount());
+    Thread thread_2 = new(() => Server.AddToCount(1));
+    Thread thread_3 = new(() => Server.GetCount());
+    thread_1.Name = $"thread {i}";
+    thread_2.Name = $"thread {i + 1}";
+    thread_3.Name = $"thread {i + 2}";
+    thread_1.Start();
+    thread_2.Start();
+    thread_3.Start();
+}
 
 
 //Задание 2
@@ -50,13 +49,13 @@ using Task_2;
 // Если timeout в методе Invoke < Thread.Sleep, делегат прерывается
 // В случае работы без прерывания, completedOk = true и выводится сообщение "Hello!"
 
-//EventHandler h = new EventHandler(TestMethod);
-//AsyncCaller ac = new(h);
-//bool completedOk = ac.Invoke(5000, null, EventArgs.Empty);
-//Console.WriteLine(completedOk);
+EventHandler h = new EventHandler(TestMethod);
+AsyncCaller ac = new(h);
+bool completedOk = ac.Invoke(5000, null, EventArgs.Empty);
+Console.WriteLine(completedOk);
 
-//void TestMethod(object? timeout, EventArgs eventArgs)
-//{
-//    Thread.Sleep(2000);
-//    Console.WriteLine("Hello!");
-//}
+void TestMethod(object? timeout, EventArgs eventArgs)
+{
+    Thread.Sleep(2000);
+    Console.WriteLine("Hello!");
+}
